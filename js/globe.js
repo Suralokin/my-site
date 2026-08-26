bd4075ee80fee8d5c34208b0a54bb108f402ea8f
suralokin123 <kolya_romashkin@bk.ru>
2026-08-26 10:22:59 +0300

    perf: optimize planet texture 4MB->391KB, add cache headers, remove unused 5.8MB photo.jpg

--- js/globe.js
+++ js/globe.js
@@ -25,7 +25,7 @@
     texData = tc.getImageData(0, 0, texW, texH).data;
     texReady = true;
   };
-  texImg.src = 'images/planet-texture.png';
+  texImg.src = 'images/planet-texture.jpg';
 
   var countryLabels = [
     {lat:60,lng:40,name:'Россия',code:'RU'},
